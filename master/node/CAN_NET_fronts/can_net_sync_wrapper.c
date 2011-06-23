49c1c844d13bd9b9549cc43b5dc19330e4500f57
Gennady.Kalashnikov <mgaldoru@gmail.com>
2011-06-23 14:48:38 +0400

    ---

---
+++ master/node/CAN_NET_fronts/can_net_sync_wrapper.c
@@ -0,0 +1,26 @@
+/*
+ * can_net_sync_wrapper.c
+ *
+ *  Created on: Jun 3, 2011
+ *      Author: mgaldoru
+ */
+
+#include "can_net_sync_wrapper.h"
+
+int can_net_send_msg_sync(/*const*/ msg_lvl2_t* msg) {
+	//TODO: can_net_send_msg_sync
+	/*callback: { x = 0 }
+	 * x = 1
+	 * while(x) { sleep(); }
+	 * return
+	 */
+}
+
+msg_lvl2_t* can_net_read_msg_sync() {
+	//TODO: can_net_read_msg_sync
+	/*callback: { y = 0; msg_read = mgs_received //(make copy) }
+	 * y = 1
+	 * while(y) { sleep(); }
+	 * return msg_read
+	 */
+}
---
+++ master/node/CAN_NET_fronts/can_net_sync_wrapper.h
@@ -0,0 +1,26 @@
+/*
+ * can_net_sync_wrapper.h
+ *
+ *  Created on: Jun 3, 2011
+ *      Author: mgaldoru
+ */
+
+#ifndef CAN_NET_SYNC_WRAPPER_H_
+#define CAN_NET_SYNC_WRAPPER_H_
+
+#include "../../../../net/trunk/can_net.h"
+
+/**
+ * can_net_start_sending_msg - start message sending
+ * @ 1) msg: message to send - const msg_lvl2_t*
+ * @return: return codes: look at callback return codes in can_net_middle.h
+ */
+int can_net_send_msg_sync(/*const*/ msg_lvl2_t* msg);
+
+/**
+ * can_net_start_sending_msg - start message sending
+ * @return: return codes: look at callback return codes in can_net_middle.h
+ */
+msg_lvl2_t* can_net_read_msg_sync();
+
+#endif /* CAN_NET_SYNC_WRAPPER_H_ */

d08d6aa9e74a32de924df5cc2871a5fad72e902f
Gennady.Kalashnikov <mgaldoru@gmail.com>
2011-07-04 20:53:58 +0400

    LVL3: heartbeat msg, header for all system msgs ids

---
+++ common/pmd_net/system_heartbeat.c
@@ -0,0 +1,10 @@
+// created 04.07.2011
+
+#include "system_heartbeat.h"
+
+int pmd_net_get_heartbeat_msg(bytearr_t * data) {
+	data->len = 0;
+	data->itself = NULL;
+}
+
+//
---
+++ common/pmd_net/system_heartbeat.h
@@ -0,0 +1,8 @@
+// created 04.07.2011
+
+#ifndef SYSTEM_HEARTBEAT_H_
+#define SYSTEM_HEARTBEAT_H_
+
+int pmd_net_get_heartbeat_msg(bytearr_t * data);
+
+#endif /* SYSTEM_HEARTBEAT_H_ */

1513ebaf8cea456f71ee1fbb7e34d9a5ff6b945e
Timofey.Kirillov <distorhead@gmail.com>
2011-07-04 14:16:27 +0400

    System config messages parser/emitter refactofing.

--- common/pmd_net/system.h
+++ common/pmd_net/system.h
@@ -21,24 +21,24 @@ typedef struct {
 /**
  * @return
  *      0 -- successful write
- *      1 -- function parameters problem: data pointer is NULL
+ *      1 -- function parameters problem: source data or destination array pointer is NULL
  *      2 -- memory allocation problem
  *      3 -- config full  command error: config pointer is NULL
  *      4 -- config full command error: failed to write config
  *      5 -- config section operation command error: section pointer is NULL
  *      6 -- config section add command error: failed to write section
  */
-int pmd_net_system_config_write_data(bytearr_t arr, const pmd_net_system_config_data_t * data);
+int pmd_net_system_config_write_data(bytearr_t * dest_arr, const pmd_net_system_config_data_t * source_data);
 
 /**
  * @return
  *      0 -- successful read
- *      1 -- function parameters problem: data pointer is NULL or byte-array is not empty
+ *      1 -- function parameters problem: source array or destination data pointer is NULL
  *      2 -- memory allocation problem
  *      3 -- config full command error: parsing failed
  *      4 -- config section add command error: parsing failed
- *      5 -- config section del command error: bad source data
+ *      5 -- bad source data
  */
-int pmd_net_system_config_read_data(const bytearr_t arr, pmd_net_system_config_data_t * data);
+int pmd_net_system_config_read_data(const bytearr_t * source_arr, pmd_net_system_config_data_t * dest_data);
 
 #endif /* PMD_NET_SYSTEM_H_ */

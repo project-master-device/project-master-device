867831a5c3535deff050d97dcb277e7e86a4115a
Timofey.Kirillov <distorhead@gmail.com>
2011-07-06 14:17:02 +0400

    Added set operation mode system message.

--- common/pmd_net/system_ids.h
+++ common/pmd_net/system_ids.h
@@ -1,6 +1,7 @@
 #ifndef SYSTEM_IDS_H_
 #define SYSTEM_IDS_H_
 
-#define PMD_NET_SYSTEM_CONFIG_MSG           1
+#define PMD_NET_SYSTEM_CONFIG           1
+#define PMD_NET_SYSTEM_SET_OP           2
 
 #endif /* SYSTEM_IDS_H_ */
---
+++ common/pmd_net/system_set_op.h
@@ -0,0 +1,35 @@
+#ifndef SYSTEM_SET_OP_H_
+#define SYSTEM_SET_OP_H_
+
+#include "../net/can_net_middle.h"
+
+#include <stdint.h>
+
+typedef struct {
+    uint8_t operation;
+} pmd_net_system_set_op_data_t;
+
+
+#define PMD_NET_SYSTEM_SET_OP_INIT              1
+#define PMD_NET_SYSTEM_SET_OP_NORMAL            2
+#define PMD_NET_SYSTEM_SET_OP_CONFIGURATION     3
+
+/**
+ * @return
+ *      0 -- successful write
+ *      1 -- function parameters problem: source data or destination array pointer are NULL
+ *      2 -- memory allocation problem
+ *      3 -- unknown operation
+ */
+int pmd_net_system_set_op_write_data(bytearr_t * dest_arr, const pmd_net_system_set_op_data_t * source_data);
+
+/**
+ * @return
+ *      0 -- successful write
+ *      1 -- function parameters problem: source data or destination array pointer are NULL
+ *      3 -- unknown operation
+ *      4 -- bad source data
+ */
+int pmd_net_system_set_op_read_data(const bytearr_t * source_arr, pmd_net_system_set_op_data_t * dest_data);
+
+#endif /* SYSTEM_SET_OP_H_ */
